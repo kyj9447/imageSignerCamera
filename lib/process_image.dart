0e6d1ef8dbcb77cdf16e9d70355283a992adf909
kyj9447 <kyj9447@gmail.com>
2024-05-27 20:33:08 +0900

    minor changes

--- lib/process_image.dart
+++ lib/process_image.dart
@@ -4,11 +4,11 @@ import 'package:image/image.dart' as img;
 import 'package:image_signer_camera/image_signer.dart';
 import 'package:image_signer_camera/save_image.dart';
 
-DateTime startTime = DateTime.now();
+//DateTime startTime = DateTime.now();
 
 // compute()용 wrapper
 void processImageWrapper(List<dynamic> args) async {
-  print("processImageWrapper 시작");
+  //print("processImageWrapper 시작");
   //[image, rotateDegree, BinaryProvider, rootIsolateToken]
 
   // 3. 토큰을 통해 isolate 초기화
@@ -24,35 +24,35 @@ void processImageWrapper(List<dynamic> args) async {
     rotateDegree = 0;
   }
   
-  print("회전각도 : $rotateDegree");
+  //print("회전각도 : $rotateDegree");
 
   // 3. 암호화 텍스트
   BinaryProvider cryptedBinary = args[2];
 
-  String startString = cryptedBinary.startString;
-  String endString = cryptedBinary.endString;
-  print("Start String : $startString");
-  print("End String : $endString");
+  //String startString = cryptedBinary.startString;
+  //String endString = cryptedBinary.endString;
+  //print("Start String : $startString");
+  //print("End String : $endString");
 
-  print('Execution time: ${DateTime.now().difference(startTime).inMilliseconds} ms');
+  //print('Execution time: ${DateTime.now().difference(startTime).inMilliseconds} ms');
 
   // 4. 이미지 처리 함수 실행
 
   // 0. 이미지 읽기
   img.Image? imageBytes = img.decodeImage(await xFileImage.readAsBytes());
-  print('이미지 읽기 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
+  //print('이미지 읽기 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
 
 
   // 1. 이미지 회전
-  print("회전각도 : $rotateDegree");
+  //print("회전각도 : $rotateDegree");
   img.Image rotatedImage = img.copyRotate(imageBytes!, angle: rotateDegree);
-  print('이미지 회전 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
+  //print('이미지 회전 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
 
   // 2-2. 이미지에 텍스트 숨기기
   img.Image signedImage = await addHiddenBit(rotatedImage, cryptedBinary);
-  print('텍스트 주입 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
+  //print('텍스트 주입 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
 
   // 3. 이미지 저장
   await saveImage(signedImage);
-  print('이미지 저장 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
+  //print('이미지 저장 : ${DateTime.now().difference(startTime).inMilliseconds} ms');
 }
